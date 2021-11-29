b7c46afdbbcf79f1944179eada73b607b5f5c7bd
Arthur Guzman <alxguzmanc@gmail.com>
2021-11-29 17:32:46 -0500

    Add groups purchases association

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,4 @@
 class Group < ApplicationRecord
+  has_and_belongs_to_many :purchases
   belongs_to :user
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,3 +1,4 @@
 class Purchase < ApplicationRecord
+  has_and_belongs_to_many :groups
   belongs_to :user
 end
