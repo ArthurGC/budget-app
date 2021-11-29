17ad4dde9ec0b70fa97cf7750b8cd63d6b8dfaf1
Arthur Guzman <alxguzmanc@gmail.com>
2021-11-29 15:30:51 -0500

    Add devise gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    before_action :update_allowed_parameters, if: :devise_controller?
+
+    def update_allowed_parameters
+        devise_parameter_sanitizer.permit(:sign_up) do |u|
+          u.permit(:name, :bio, :photo, :email, :password, :password_confirmation)
+        end
+    
+        devise_parameter_sanitizer.permit(:account_update) do |u|
+          u.permit(:name, :bio, :photo, :email, :password, :current_password)
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :users
+  root "devise/sessions#new"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
