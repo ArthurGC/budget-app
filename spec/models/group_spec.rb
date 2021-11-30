6a73f7559833917fa1cfd2064b4631216966e61b
Arthur Guzman <alxguzmanc@gmail.com>
2021-11-30 15:12:33 -0500

    Add purchase tests

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Group, type: :model do
   before(:each) do
     @user = create(:user)
     @group = create(:group, author_id: @user.id)
+    @purchase1 = create(:purchase, author_id: @user.id)
+    @purchase2 = create(:purchase, author_id: @user.id)
   end
 
   it 'name should be present' do
@@ -14,6 +16,12 @@ RSpec.describe Group, type: :model do
   it 'icon should be present' do
     @group.icon = nil
     expect(@group).to_not be_valid
+  end
+
+  it 'total_amount should be the total amount of purchases' do
+    @group.purchases << @purchase1
+    @group.purchases << @purchase2
+    expect(@group.total_amount).to eq(@purchase1.amount + @purchase2.amount)
   end
 
 end
