e8cb63c15c2071f435cf809157b317e2bd5f6c9e
Arthur Guzman <alxguzmanc@gmail.com>
2021-11-30 10:41:24 -0500

    Fix strong parameters in devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,11 +11,11 @@ class ApplicationController < ActionController::Base
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:name, :bio, :photo, :email, :password, :password_confirmation)
+      u.permit(:name, :email, :password, :password_confirmation)
     end
 
     devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:name, :bio, :photo, :email, :password, :current_password)
+      u.permit(:name, :email, :password, :current_password)
     end
   end
 end
