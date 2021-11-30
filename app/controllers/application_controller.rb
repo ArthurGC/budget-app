64ea2b6e364853ef6da6062b7d6ddacc51b5fe95
Arthur Guzman <alxguzmanc@gmail.com>
2021-11-29 21:51:05 -0500

    Edit redirect when User Sign_in or Sign_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
     before_action :update_allowed_parameters, if: :devise_controller?
 
+    def after_sign_in_path_for(resources)
+      groups_path
+    end
+
+    def after_sign_up_path_for(resources)
+      groups_path
+    end
+
     def update_allowed_parameters
         devise_parameter_sanitizer.permit(:sign_up) do |u|
           u.permit(:name, :bio, :photo, :email, :password, :password_confirmation)
