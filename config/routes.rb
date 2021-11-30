a05a62a2350ec4c16eb24b96d1bc0172b3c7de6f
Arthur Guzman <alxguzmanc@gmail.com>
2021-11-29 19:31:20 -0500

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  resources :purchases
-  resources :groups
   devise_for :users
-  resources :users
+  resources :groups do
+    resources :purchases
+  end
   root 'users#main_page'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
