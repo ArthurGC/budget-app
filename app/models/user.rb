8518ef9df6a54f327239bb3ca8fae5c42661d069
Arthur Guzman <alxguzmanc@gmail.com>
2021-11-29 22:21:54 -0500

    Add associations has_many in Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :groups, dependent: :destroy, foreign_key: 'author_id'
+  has_many :purchases, dependent: :destroy, foreign_key: 'author_id'
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
