adb7668af09f5ab3ca9ba9f1000f43d1d7976241
Arthur Guzman <alxguzmanc@gmail.com>
2021-11-29 21:53:46 -0500

    Fix linter's issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,21 @@
 class ApplicationController < ActionController::Base
-    before_action :update_allowed_parameters, if: :devise_controller?
+  before_action :update_allowed_parameters, if: :devise_controller?
 
-    def after_sign_in_path_for(resources)
-      groups_path
+  def after_sign_in_path_for(_resources)
+    groups_path
+  end
+
+  def after_sign_up_path_for(_resources)
+    groups_path
+  end
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :bio, :photo, :email, :password, :password_confirmation)
     end
 
-    def after_sign_up_path_for(resources)
-      groups_path
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :bio, :photo, :email, :password, :current_password)
     end
-
-    def update_allowed_parameters
-        devise_parameter_sanitizer.permit(:sign_up) do |u|
-          u.permit(:name, :bio, :photo, :email, :password, :password_confirmation)
-        end
-    
-        devise_parameter_sanitizer.permit(:account_update) do |u|
-          u.permit(:name, :bio, :photo, :email, :password, :current_password)
-        end
-    end
+  end
 end
