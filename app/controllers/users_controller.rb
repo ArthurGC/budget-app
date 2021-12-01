ee3df2197e2a2ce26ff6e2c7cbbd7241a8ba388c
Arthur Guzman <alxguzmanc@gmail.com>
2021-12-01 15:03:35 -0500

    Add icon select tag and improve styles in main page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy]
 
   def main_page
+    @current_user = current_user
     authorize! :read, User
   end
 
--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,6 @@ class Group < ApplicationRecord
   has_and_belongs_to_many :purchases, dependent: :destroy
   belongs_to :author, class_name: 'User'
 
-  validates_uniqueness_of :name
   validates :name, :icon, presence: true
 
   def total_amount
