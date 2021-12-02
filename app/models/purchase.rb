1a0a251c08c7a279a8ae4ad489e91505c536a5d5
Arthur Guzman <alxguzmanc@gmail.com>
2021-12-02 02:17:29 -0500

    Remove dependant_destroy from purchases

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,5 +1,5 @@
 class Purchase < ApplicationRecord
-  has_and_belongs_to_many :groups, dependent: :destroy
+  has_and_belongs_to_many :groups
   belongs_to :author, class_name: 'User'
 
   validates :name, :amount, presence: true
