02285f81c61849a98530e245821ab8143d16b2e5
Arthur Guzman <alxguzmanc@gmail.com>
2021-12-02 02:18:17 -0500

    Remove unnecessary comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,11 +10,6 @@ class ApplicationController < ActionController::Base
   end
 
   rescue_from CanCan::AccessDenied do |exception|
-    # respond_to do |format|
-    #   format.json { render nothing: true, status: :not_found }
-    #   format.html { redirect_to main_app.root_url, notice: exception.message, status: :not_found }
-    #   format.js   { render nothing: true, status: :not_found }
-    # end
     redirect_to root_url, alert: exception.message
   end
 
